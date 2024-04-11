7b2dcb73d7932f658b37f0033e9f8fb2c2e64166
soma <ogbennaanita@gmail.com>
2024-04-11 16:50:13 +0100

    updates

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
+'''A module for generating Pascal's triangle.
+'''
+
 
 def pascal_triangle(n):
-    result = []
+    '''Generates Pascal's triangle up to the nth row.
+
+    Args:
+        n (int): The number of rows to generate.
+
+    Returns:
+        list: A list of lists representing Pascal's triangle.
+    '''
+    triangle = []
     if type(n) is not int or n <= 0:
-        return result
-    for i in range(n):
+        return triangle
+    
+    # Generate each row of Pascal's triangle
+    for row_index in range(n):
         row = []
-        for j in range(i + 1):
-            if j == 0 or j == i:
+        for col_index in range(row_index + 1):
+            # First and last elements in each row are always 1
+            if col_index == 0 or col_index == row_index:
                 row.append(1)
-            elif i > 0 and j > 0:
-                row.append(result[i - 1][j - 1] + result[i - 1][j])
-        result.append(row)
-    return result
+            # Calculate other elements based on the values from the previous row
+            elif row_index > 0 and col_index > 0:
+                row.append(triangle[row_index - 1][col_index - 1] + triangle[row_index - 1][col_index])
+        triangle.append(row)
+    return triangle
 
