f35f8f01206b93d6ba1c1b1b470944fc429f0b90
soma <ogbennaanita@gmail.com>
2024-04-25 19:22:14 +0100

    updates

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+"""0. Minimum Operations"""
 
 def minOperations(n):
     """
@@ -11,20 +12,16 @@ def minOperations(n):
     Returns:
         int: The fewest number of operations needed to reach n H characters. If n is impossible to achieve, returns 0.
     """
-    if n <= 1:
-        return n
 
-    # Initialize variables
+    if not isinstance(n, int):
+        return 0
+
     operations = 0
-    clipboard = 1
-    buffer = 1
-
-    while buffer < n:
-        if n % buffer == 0:
-            clipboard = buffer
-            operations += 1
-        buffer += clipboard
-        operations += 1
-
+    iterator = 2
+    while (iterator <= n):
+        if not (n % iterator):
+            n = int(n / iterator)
+            operations += iterator
+            iterator = 1
+        iterator += 1
     return operations
-
