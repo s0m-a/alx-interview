7fa84073b17d0e1eaf638d13736f9c55bead8a0f
soma <ogbennaanita@gmail.com>
2024-05-23 21:54:46 +0100

    updates

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const util = require('util');
+const request = util.promisify(require('request'));
+const filmID = process.argv[2];
+
+async function starwarsCharacters (filmId) {
+  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+  let response = await (await request(endpoint)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
+
+  for (let i = 0; i < characters.length; i++) {
+    const urlCharacter = characters[i];
+    let chters = await (await request(urlCharacter)).body;
+    chters = JSON.parse(character);
+    console.log(chters.name);
+  }
+}
+
+starwarsCharacters(filmID);
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,1 @@
+this is a read me file for :0x06. Star Wars API
