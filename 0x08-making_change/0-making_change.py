96f365b7726f62f919c39530f4394190774414c4
soma <ogbennaanita@gmail.com>
2024-06-07 03:35:48 +0100

    updates

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,29 +1,29 @@
-#!/usr/bin/python3 
-""" Making changes """
+#!/usr/bin/python3
+"""Making changes"""
 
 
-def make_change(coins, total):
+def make_change(coins, target):
     """
     Generate changes needed to reach total
     
     Args:
-        coins ([List]): [List of Coins available]
-        total ([int]): [total amount needed]
+        coins (List[int]): List of coin denominations
+        target (int): Target amount to reach
+    
+    Returns:
+        int: Fewest number of coins needed to meet the target amount,
+             or -1 if it's not possible to make the target amount with the available coins
     """
+    table = [float('inf')] * (target + 1)
+    table[0] = 0  # Base case: 0 coins needed to make an amount of 0
+    
+    for amount in range(1, target + 1):
+        for coin in coins:
+            if coin <= amount:
+                table[amount] = min(table[amount], table[amount - coin] + 1)
+    
+    if table[target] == float('inf'):
+        return -1
+    else:
+        return table[target]
 
-    if total <= 0:
-        return 0
-
-    coins.sort(reverse=True)
-    current_sum = 0
-    coin_index = 0
-    coin_count = 0
-    num_coins = len(coins)
-    while current_sum < total and coin_index < num_coins:
-        while coins[coin_index] <= total - current_sum:
-            current_sum += coins[coin_index]
-            coin_count += 1
-            if current_sum == total:
-                return coin_count
-        coin_index += 1
-    return -1
