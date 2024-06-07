022ebe384fdb4c0ae4df6e21ccc4eb3702cfef3c
soma <ogbennaanita@gmail.com>
2024-06-07 03:31:54 +0100

    updates

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,23 +1,29 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 
 """ Making changes """
 
 
 def make_change(coins, total):
     """
     Generate changes needed to reach total
-
+    
     Args:
         coins ([List]): [List of Coins available]
         total ([int]): [total amount needed]
     """
+
     if total <= 0:
         return 0
 
-    check = [float('inf')] * (total + 1)
-    check[0] = 0
-
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            check[amount] = min(check[amount], check[amount - coin] + 1)
-
-    return check[total] if check[total] != float('inf') else -1
+    coins.sort(reverse=True)
+    current_sum = 0
+    coin_index = 0
+    coin_count = 0
+    num_coins = len(coins)
+    while current_sum < total and coin_index < num_coins:
+        while coins[coin_index] <= total - current_sum:
+            current_sum += coins[coin_index]
+            coin_count += 1
+            if current_sum == total:
+                return coin_count
+        coin_index += 1
+    return -1
